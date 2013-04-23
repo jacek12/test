1844c842a4fca875139a6f22c19691b08ea9bfe8
Jan Kowalski <student@s108-00-u.(none)>
2013-04-23 18:37:26 +0200

    dodanie testu

---
+++ test1.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+
+void test1(int n);
+void test2(int n);
+
+int main(){
+    //jakis komentarz
+    printf("test");
+    return 0;
+}
+
+void test1(int n){
+    printf("jakis tekst");
+}
